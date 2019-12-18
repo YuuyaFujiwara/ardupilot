c7ad99f124de7087767a090b5f410e053b7f6968
Yuuya Fujiwara <y.fujiwara@tohotechnos.co.jp>
2019-12-18 09:35:24 +0900

    不要部分の削除

--- libraries/AP_Momimaki/AP_Momimaki.h
+++ libraries/AP_Momimaki/AP_Momimaki.h
@@ -18,20 +18,6 @@
 
 
 
-#if false
-    #define AP_CAMERA_TRIGGER_TYPE_SERVO                0
-    #define AP_CAMERA_TRIGGER_TYPE_RELAY                1
-
-    #define AP_CAMERA_TRIGGER_DEFAULT_TRIGGER_TYPE  AP_CAMERA_TRIGGER_TYPE_SERVO    // default is to use servo to trigger camera
-
-    #define AP_CAMERA_TRIGGER_DEFAULT_DURATION  10      // default duration servo or relay is held open in 10ths of a second (i.e. 10 = 1 second)
-
-    #define AP_CAMERA_SERVO_ON_PWM              1300    // default PWM value to move servo to when shutter is activated
-    #define AP_CAMERA_SERVO_OFF_PWM             1100    // default PWM value to move servo to when shutter is deactivated
-
-    #define AP_CAMERA_FEEDBACK_DEFAULT_FEEDBACK_PIN -1  // default is to not use camera feedback pin
-#endif
-
 /// @class	Momimaki
 /// @brief	Object managing momimaki unit
 class AP_Momimaki {
@@ -64,33 +50,8 @@ public:
     void control(int8_t enable_spreader, int8_t enable_feeder, float spread_radius, float spread_density );
 
 
-#if false
-    // MAVLink methods
-    void            control_msg(const mavlink_message_t &msg);
-    void            send_feedback(mavlink_channel_t chan);
-
-    // Command processing
-    void            configure(float shooting_mode, float shutter_speed, float aperture, float ISO, float exposure_type, float cmd_id, float engine_cutoff_time);
-    // handle camera control
-    void            control(float session, float zoom_pos, float zoom_step, float focus_lock, float shooting_cmd, float cmd_id);
-
-    // set camera trigger distance in a mission
-    void            set_trigger_distance(uint32_t distance_m)
-    {
-        _trigg_dist.set(distance_m);
-    }
-
-    void take_picture();
-#endif
-
-
     // Update - to be called periodically @at least 10Hz
     void update();
-
-#if false
-    // update camera trigger - 50Hz
-    void update_trigger();
-#endif
 
     static const struct AP_Param::GroupInfo        var_info[];
 
@@ -99,21 +60,7 @@ public:
     {
         _is_in_auto_mode = enable;
     }
-#if false
 
-    void set_mode( Mode::Number arg_new_mode )
-    {
-        _mode_number = arg_new_mode;
-    }
-#endif
-
-#if false
-
-    enum camera_types {
-        CAMERA_TYPE_STD,
-        CAMERA_TYPE_BMMCC
-    };
-#endif
 
 private:
 
@@ -154,66 +101,6 @@ private:
     float _density;
     float _radius;
 
-
-#if false
-    // 莉･荳九�、P_Camera縺ｮ繝代Λ繝｡繝ｼ繧ｿ
-    AP_Int8         _trigger_type;      // 0:Servo,1:Relay
-    AP_Int8         _trigger_duration;  // duration in 10ths of a second that the camera shutter is held open
-    AP_Int8         _relay_on;          // relay value to trigger camera
-    AP_Int16        _servo_on_pwm;      // PWM value to move servo to when shutter is activated
-    AP_Int16        _servo_off_pwm;     // PWM value to move servo to when shutter is deactivated
-    uint8_t         _trigger_counter;   // count of number of cycles shutter has been held open
-    uint8_t         _trigger_counter_cam_function;   // count of number of cycles alternative camera function has been held open
-    AP_Int8         _auto_mode_only;    // if 1: trigger by distance only if in AUTO mode.
-    AP_Int8         _type;              // Set the type of camera in use, will open additional parameters if set
-    //bool            _is_in_auto_mode;   // true if in AUTO mode
-
-    void            servo_pic();        // Servo operated camera
-    void            relay_pic();        // basic relay activation
-    void            feedback_pin_timer();
-    void            feedback_pin_isr(uint8_t, bool, uint32_t);
-    void            setup_feedback_callback(void);
-
-    AP_Float        _trigg_dist;        // distance between trigger points (meters)
-    AP_Int16        _min_interval;      // Minimum time between shots required by camera
-    AP_Int16        _max_roll;          // Maximum acceptable roll angle when trigging camera
-    uint32_t        _last_photo_time;   // last time a photo was taken
-    struct Location _last_location;
-    uint16_t        _image_index;       // number of pictures taken since boot
-
-    // pin number for accurate camera feedback messages
-    AP_Int8         _feedback_pin;
-    AP_Int8         _feedback_polarity;
-
-    uint32_t        _camera_trigger_count;
-    uint32_t        _camera_trigger_logged;
-    uint32_t        _feedback_timestamp_us;
-    bool            _timer_installed;
-    bool            _isr_installed;
-    uint8_t         _last_pin_state;
-
-    void log_picture();
-
-//    uint32_t log_camera_bit;
-    const struct Location &current_loc;
-    
-    
-    
-    
-
-    // entry point to trip local shutter (e.g. by relay or servo)
-    void trigger_pic();
-
-    // de-activate the trigger after some delay, but without using a delay() function
-    // should be called at 50hz from main program
-    void trigger_pic_cleanup();
-
-    // return true if we are using a feedback pin
-    bool using_feedback_pin(void) const
-    {
-        return _feedback_pin > 0;
-    }
-#endif
 };
 
 namespace AP {
