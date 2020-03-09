a271b6d0aeb06aec6a9f771b0311ba31289844ea
Yuuya Fujiwara <y.fujiwara@tohotechnos.co.jp>
2020-03-09 09:54:43 +0900

    RCOutputRGBLedInverted

--- libraries/AP_Notify/AP_Notify.cpp
+++ libraries/AP_Notify/AP_Notify.cpp
@@ -244,7 +244,7 @@ void AP_Notify::add_backends(void)
 #endif
 #else
                 //QL44 強制的に設定(NAVIO2+RCOutputRGBLed）
-                ADD_BACKEND(new RCOutputRGBLed(11, 12, 13));//QL44
+                ADD_BACKEND(new RCOutputRGBLedInverted(11, 12, 13));//QL44
                 ADD_BACKEND(new Led_Sysfs("rgb_led0", "rgb_led2", "rgb_led1"));
 #endif
                 break;
